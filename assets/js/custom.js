dad1d9b988699b2d1d3e5cd1273b6d5e36e9a226
dereknutile <dereknutile@gmail.com>
2015-03-25 20:10:41 -0700

    Adds simple fade in/out descriptive text to the landing page.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,17 @@
 
 $(document).ready(function(){
-  // cool code goes here!
+
+  var text = $(".swap-text");
+  var textIndex = -1;
+
+  function swapText() {
+    ++textIndex;
+    text.eq(textIndex % text.length)
+      .fadeIn(900)
+      .delay(1200)
+      .fadeOut(1200, swapText);
+  }
+
+  swapText();
+
 });
