2c841f47240c0b5ecc332e7f550d3329df6bcf75
Derek Nutile <dereknutile@gmail.com>
2016-03-20 08:40:07 -0700

    Removes everything from the custom.js file except the filter.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,40 +1,5 @@
 $(document).ready(function(){
 
-  // var text = $(".swap-text");
-  // var textIndex = -1;
-  //
-  // function swapText() {
-  //   ++textIndex;
-  //   text.eq(textIndex % text.length)
-  //     .fadeIn(1400)
-  //     .delay(1600)
-  //     .fadeOut(900, swapText);
-  // }
-  //
-  // swapText();
+    $('input#filter-skills').quicksearch('.skills-row');
 
-   $('input#filter-skills').quicksearch('.skills-row');
-
-  // jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
-      if ($(".navbar").offset().top > 50) {
-          $(".navbar-fixed-top").addClass("top-nav-collapse");
-      } else {
-          $(".navbar-fixed-top").removeClass("top-nav-collapse");
-      }
-  });
-
-  $(function() {
-      $('a.page-scroll').bind('click', function(event) {
-          var $anchor = $(this);
-          $('html, body').stop().animate({
-              scrollTop: $($anchor.attr('href')).offset().top
-          }, 1500, 'easeInOutExpo');
-          event.preventDefault();
-      });
-  });
-
-  $('.navbar-collapse ul li a').click(function() {
-      $('.navbar-toggle:visible').click();
-  });
 });
