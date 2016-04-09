f568392e03254a8ba1d9db3f3f9b1e7a18884be0
Derek Nutile <dereknutile@gmail.com>
2016-04-09 09:55:49 -0700

    Adds javascript to fadeIn/Out the logo.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
 
     $('input#filter-skills').quicksearch('.skills-row');
 
+    $(window).on('scroll', function() {
+        var offset = window.pageYOffset;
+        var position = 256;
+
+        if(offset > position) {
+            $('.side-logo').fadeIn( "slow" );
+        } else {
+            $('.side-logo').fadeOut("slow" );
+        }
+    });
+
 });
